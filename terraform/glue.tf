84bda3008f2db6f6973e68b0e864aa83b72aca63
Buya0720 <buyandelger.batu@gmail.com>
2025-08-31 16:35:50 +1000

    fix

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -7,10 +7,8 @@ resource "aws_glue_catalog_database" "clean" {
     Env = "dev"
   }
 }
- 
 
- resource "aws_s3_object" "clean_directory" {
-    bucket = module.data_bucket.bucket_name
-    key = "/clean"
-    content = ""
- }
+resource "aws_s3_object" "clean_directory" {
+  bucket  = module.data_bucket.bucket_name
+  key     = "clean/"
+}

558c2c9d2d9f946690770aab8a4d1f1200c7c8aa
Buya0720 <buyandelger.batu@gmail.com>
2025-09-01 20:05:13 +1000

    fix

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -18,25 +18,19 @@ resource "aws_instance" "this" {
   }
 }
 
-resource "terraform_data" "provision_every_apply" {
+resource "null_resource" "remote_commands" {
   count = var.airflow_scripts!="" ? 1 : 0
-  # keep this tied to your instance so if it’s replaced, this re-runs too
-  input = {
-    instance_id = aws_instance.this.id
-  }
-
-  # the magic: any change here forces replacement → provisioners run again
-  triggers_replace = timestamp()
 
   connection {
+    type        = "ssh"
     host        = aws_instance.this.public_ip
-    user        = "ec2-user"          # or "ubuntu", etc
+    user        = "ec2-user"
     private_key = var.ssh_private_key
   }
 
   provisioner "remote-exec" {
     inline = [
-      "echo 'Hello from remote-exec at $(date)' | sudo tee /tmp/hello.txt",
+      "echo 'Executing remote-exec provisioner...'",
       "${var.airflow_scripts}"
     ]
   }

feab18bb3d21d67e5988c67b1fb3caf3b58268eb
Buya0720 <buyandelger.batu@gmail.com>
2025-09-04 18:41:55 +1000

    3-sep , wednesday

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "this" {
   iam_instance_profile        = aws_iam_instance_profile.profile.name
   associate_public_ip_address = true
 
-  key_name                    = "buyadeb"
+  key_name                    = "buya-deb"
   user_data                   = local.user_data
 
   private_ip                  = var.private_ip
@@ -38,6 +38,7 @@ resource "null_resource" "remote_commands" {
     inline = [
       "echo 'Executing remote-exec provisioner...'",
       "${var.airflow_scripts}",
+      "su - airflow -c \"set -a; source /etc/airflow/airflow.env; set +a; source ~/venv/bin/activate; airflow dags reserialize\"",
       "b64='${base64encode(local.airflow_conn)}'; set +e; printf %s \"$b64\" | base64 -d | bash -s; rc=$?; if [ $rc -eq 141 ]; then echo 'Ignoring benign SIGPIPE (141)'; exit 0; else exit $rc; fi"
     ]
   }

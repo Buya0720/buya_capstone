fe7a38410032c554dfc5dd8bab87af5f815e39ca
Buya0720 <buyandelger.batu@gmail.com>
2025-09-12 21:07:19 +1000

    fix

--- dbt/models/warehouse/warehouse_customers.sql
+++ dbt/models/warehouse/warehouse_customers.sql
@@ -25,8 +25,7 @@ WITH customers AS (
         employment_status,
         credit_score,
         primary_bank,
-        primary_branch,
-        load_date
+        primary_branch
     FROM
         {{ ref('customers') }}
 )
@@ -48,6 +47,5 @@ SELECT
     employment_status,
     credit_score,
     primary_bank,
-    primary_branch,
-    load_date
+    primary_branch
 FROM customers c 

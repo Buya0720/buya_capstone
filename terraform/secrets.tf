56bad7dba2878e85190ca3d08ce05817d6f12c05
Buya0720 <buyandelger.batu@gmail.com>
2025-09-22 19:19:37 +1000

    fix

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -4,6 +4,6 @@ resource "aws_secretsmanager_secret" "youtube_secret" {
 }
 
 resource "aws_secretsmanager_secret_version" "youtube_secret_version" {
-  secret_id = aws_secretsmanager_secret.youtube_secret.id
+  secret_id = aws_secretsmanager_secret.youtube_secret2.id
   secret_string = var.yt_api_key
 }
